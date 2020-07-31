0f8865280b0bccddb715f7d7035a7ec0459f8207
Joel Armendariz <joelarmendariz@Joels-MBP.attlocal.net>
2020-07-30 21:23:52 -0500

    feat(counter): first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="count">{count}</div>
+        <div className="btn">
+          <Fab
+            className="btn"
+            disableRipple
+            onClick={() => setCount(count + 1)}
+            color="primary"
+            aria-label="add">
+            <AddIcon />
+          </Fab>
+        </div>
+        <div className="btn">
+          <Fab
+            disableRipple
+            onClick={() => (count > 0 ? setCount(count - 1) : null)}
+            color="primary"
+            aria-label="remove">
+            <RemoveIcon />
+          </Fab>
+        </div>
+        <div className="reset-button" onClick={() => setCount(0)}>
+          Reset
+        </div>
       </header>
     </div>
   );
-}
+};
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       count: 0,
+//     };
+//     this.handleIncrement = this.handleIncrement.bind(this);
+//     this.handleDecrement = this.handleDecrement.bind(this);
+//     this.handleReset = this.handleReset.bind(this);
+//   }
+//   handleIncrement() {
+//     this.setState({ count: this.state.count + 1 });
+//   }
+
+//   handleDecrement() {
+//     if (this.state.count > 0) {
+//       this.setState({ count: this.state.count - 1 });
+//     }
+//   }
+
+//   handleReset() {
+//     this.setState({ count: 0 });
+//   }
+
+//   render() {
+//     const { count } = this.state;
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <div className="count">{count}</div>
+//           <div className="btn">
+//             <Fab
+//               className="btn"
+//               disableRipple
+//               onClick={this.handleIncrement}
+//               color="primary"
+//               aria-label="add">
+//               <AddIcon />
+//             </Fab>
+//           </div>
+//           <div className="btn">
+//             <Fab disableRipple onClick={this.handleDecrement} color="primary" aria-label="remove">
+//               <RemoveIcon />
+//             </Fab>
+//           </div>
+//           <div className="reset-button" onClick={this.handleReset}>
+//             Reset
+//           </div>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
