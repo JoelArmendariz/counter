d361869d36bc17235d131f85ac08a2987f27e81a
Joel Armendariz <joelarmendariz@Joels-MBP.attlocal.net>
2020-07-30 21:22:10 -0500

    Updates

---
+++ precache-manifest.8818f87a0d4e41cc990cdfc7939d9428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841a13195332bf22393811179a12a076",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "3fe166f58fa14c897e9b",
+    "url": "/counter/static/css/main.c87f0412.chunk.css"
+  },
+  {
+    "revision": "1c158e36f904d5d0a064",
+    "url": "/counter/static/js/2.4d652c49.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/counter/static/js/2.4d652c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fe166f58fa14c897e9b",
+    "url": "/counter/static/js/main.5256dac2.chunk.js"
+  },
+  {
+    "revision": "f0efe914179fd241fe39",
+    "url": "/counter/static/js/runtime-main.60e3ae63.js"
+  }
+]);
